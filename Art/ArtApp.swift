6a28856ac125ff883885e82ae35445b6771c2679
Evgeni Glushko <sstawr@gmail.com>
2024-03-19 19:27:13 +0300

    Initial Commit

---
+++ Art/ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtApp.swift
+//  Art
+//
+//  Created by Evgeni Glushko on 19.03.24.
+//
+
+import SwiftUI
+
+@main
+struct ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
